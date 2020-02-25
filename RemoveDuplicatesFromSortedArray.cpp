82cb8201551f89408c0cb72883c91e5803c1b645
JIN <a1824jj@aiit.ac.jp>
2020-02-25 04:07:20 +0000

    20200225

---
+++ RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,33 @@
+#include <iostream>   // std::cout, std::endl;
+#include <algorithm>  // std::sort, std::unique
+#include <vector>     // std::vector
+#include <stdio.h>    // printf
+#include <map>
+#include <queue>
+#include <string.h> //strcmp、strncmp関数を使うために必要
+#include <stdexcept>
+#include <climits>
+#include <sstream> // std::stringstream
+#include <istream> // std::getline
+#include <regex>
+#include <unordered_set>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        return nums.size();
+    }
+};
+
+int main(int argc, char* argv[]){
+    
+    Solution s;
+    vector<int> nums = {1,1,2};
+    cout << s.removeDuplicates(nums) << endl;
+    
+    return 0;
+}
